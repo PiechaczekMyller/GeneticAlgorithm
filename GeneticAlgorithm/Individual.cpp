79f74839a70cde4af88d87e8e65390962b3d18e7
Michał Myller <michalmyll@gmail.com>
2016-12-20 17:28:08 +0100

    replace Indyvidual with Individual

--- GeneticAlgorithm/Individual.cpp
+++ GeneticAlgorithm/Individual.cpp
@@ -29,3 +29,52 @@ void Individual::setFitnessScore(double &accuracy, long sizeOfTrainingSet){
 	double tempSizeOfIndyvidual = sizeOfIndividual;
 	fitnessScore=1-(tempSizeOfIndyvidual/tempSizeOfTrainingSet);
 }
+
+void crossoverIndividual(double crossoverRatio, Individual &firstIndividual, Individual &secondIndividual){
+	double randomRatio = (rand() % 100)/100;
+	if (randomRatio < crossoverRatio){
+		std::vector<std::vector<double>> tempVectorOfFeatures;
+		std::vector<std::vector<double>> tempVectorOfLabels;
+		tempVectorOfFeatures = firstIndividual.getFeaturesVector();
+		tempVectorOfFeatures.insert(tempVectorOfFeatures.end(),
+									secondIndividual.getFeaturesVector().begin(),
+									secondIndividual.getFeaturesVector().end());
+		tempVectorOfLabels = firstIndividual.getLabelsVector();
+		tempVectorOfLabels.insert(tempVectorOfLabels.end(),
+								  secondIndividual.getLabelsVector().begin(),
+								  secondIndividual.getLabelsVector().end());
+		firstIndividual.getFeaturesVector().clear();
+		firstIndividual.getLabelsVector().clear();
+		long int numberOfAClassFeatures = 0;
+		long int numberOfBClassFeatures = 0;
+		for(auto &label: tempVectorOfLabels){
+			if(label[0] == 1){
+				numberOfAClassFeatures++;
+			}
+			if(label[0] == 0){
+				numberOfBClassFeatures++;
+			}
+		}
+		long int numberOfPickedExamples = 0;
+		while(numberOfPickedExamples <= numberOfAClassFeatures / 2){
+			long randomNumber = (rand() % tempVectorOfFeatures.size());
+			if(tempVectorOfLabels[randomNumber][0] == 1){
+				firstIndividual.getFeaturesVector().push_back(tempVectorOfFeatures[randomNumber]);
+				firstIndividual.getLabelsVector().push_back(tempVectorOfLabels[randomNumber]);
+				numberOfPickedExamples++;
+			}
+		}
+		numberOfPickedExamples = 0;
+		while(numberOfPickedExamples <= numberOfBClassFeatures / 2){
+			long randomNumber = (rand() % tempVectorOfLabels.size());
+			if(tempVectorOfLabels[randomNumber][0] == 0){
+				firstIndividual.getFeaturesVector().push_back(tempVectorOfFeatures[randomNumber]);
+				firstIndividual.getLabelsVector().push_back(tempVectorOfLabels[randomNumber]);
+				numberOfPickedExamples++;
+			}
+			secondIndividual.getFeaturesVector() = tempVectorOfFeatures;
+			secondIndividual.getLabelsVector() = tempVectorOfLabels;
+		}
+	}
+
+}
--- GeneticAlgorithm/Individual.h
+++ GeneticAlgorithm/Individual.h
@@ -38,6 +38,8 @@ public:
 
 	void setFitnessScore(double &accuracy, long sizeOfTrainingSet);
 
+	friend void crossoverIndividual(double crossoverRatio, Individual &firstIndividual, Individual &secondIndividual);
+
 	virtual ~Individual(){ };
 
     void ChangeFeature(int index, vector<double> new_feature);
